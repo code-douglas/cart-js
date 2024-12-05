d1eac1481a477b2392d3ef389699cbd6fd31fec9
Douglas <douglasd3v.contato@gmail.com>
2024-12-05 20:18:31 -0300

    Adicionando arquivo de configuração do webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  devtool: process.env.NODE_ENV == 'development' ? 'source-map' : '',
+  entry: {
+    app: ['./src/app.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
